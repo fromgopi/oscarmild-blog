7e5d2af67ccdd816b1f564dba4e8b730b01d5526
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-11-08 22:14:23 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         port: 9000, //default port: 8080
     },
     module: {
-        rules: [
+        rules: [ 
             {
                 test: /\.tsx?$/,
                 use: {
